1195e4680e37e7039c647680d2937984bcd644aa
Miller Carreño <macarrenob@poligran.edu.co>
2016-02-18 21:59:37 -0500

    Programa prueba - suma numeros

--- src/ordena15nummaymen/Main.java
+++ src/ordena15nummaymen/Main.java
@@ -13,6 +13,8 @@ public class Main extends JFrame{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        //COMENTO ACÁ PARA INDICAR UN CAMBIO
         OrdenamientoNumeros ordena = new OrdenamientoNumeros();
         ordena.setVisible(true);
         ordena.setLocationRelativeTo(null);
